096ee2926653e33436c7c6f93c3d931fc8f93807
Florian Nücke <florian@nuecke.de>
2014-02-04 16:12:23 +0100

    removed unnecessary variable and added a note on the return type in getEntities

--- src/li/cil/oc/example/tileentity/TileEntityRadar.java
+++ src/li/cil/oc/example/tileentity/TileEntityRadar.java
@@ -70,7 +70,6 @@ public class TileEntityRadar extends TileEntityEnvironment {
             for (Object obj : getWorldObj().getEntitiesWithinAABB(EntityLivingBase.class, bounds)) {
                 EntityLivingBase entity = (EntityLivingBase) obj;
                 double dx = entity.posX - (xCoord + 0.5);
-                double dy = entity.posY - (yCoord + 0.5);
                 double dz = entity.posZ - (zCoord + 0.5);
                 // Check if the entity is actually in range.
                 if (Math.sqrt(dx * dx + dz * dz) < RadarRange) {
@@ -88,6 +87,13 @@ public class TileEntityRadar extends TileEntityEnvironment {
             // expensive). The time is specified in seconds.
             context.pause(0.5);
         }
+
+        // The returned array is treated as a tuple, meaning if we return the
+        // entities as an array directly, we'd end up with each entity as an
+        // individual result value (i.e. in Lua we'd have to write
+        //   result = {radar.getEntities()}
+        // and we'd be limited in the number of entities, due to the limit of
+        // return values. So we wrap it in an array to return it as a list.
         return new Object[]{entities.toArray()};
     }
 }
